00e28f4663756753d1a403337e5ecdb7274d4739
RYAN <126591328+ryanmarcelos@users.noreply.github.com>
2025-05-15 09:47:17 -0300

    Add files via upload

---
+++ simulador_carro.js
@@ -0,0 +1,26 @@
+class Carro {
+    constructor(modelo, cor) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.velocidadeAtual = 0;
+    }
+
+    acelerar(valor) {
+        this.velocidadeAtual += valor;
+        this.mostrarStatus();
+    }
+
+    frear(valor) {
+        this.velocidadeAtual = Math.max(0, this.velocidadeAtual - valor);
+        this.mostrarStatus();
+    }
+
+    mostrarStatus() {
+        console.log(`O carro ${this.modelo} (${this.cor}) está a ${this.velocidadeAtual} km/h.`);
+    }
+}
+
+
+const c = new Carro("Civic", "preto");
+c.acelerar(30); 
+//c.frear(10);   
